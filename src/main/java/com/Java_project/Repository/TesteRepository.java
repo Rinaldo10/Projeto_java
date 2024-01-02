e41501cc5a3b572839768e77b7f03fcafc47c179
Rinaldo Reverte Mendes Junior <rinaldojunior2012@gmail.com>
2024-01-01 21:00:14 -0400

    feat: Adicionado repository configs.

--- src/main/java/com/Java_project/Repository/TesteRepository.java
+++ src/main/java/com/Java_project/Repository/TesteRepository.java
@@ -1,7 +1,11 @@
 package com.Java_project.Repository;
 
+import com.Java_project.Model.Objeto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TesteRepository {
+public abstract class TesteRepository extends JpaRepository<Objeto, Long> {
+
+
 }
--- src/main/java/com/Java_project/Service/Impl/TesteService.java
+++ src/main/java/com/Java_project/Service/Impl/TesteService.java
@@ -1,7 +1,15 @@
 package com.Java_project.Service.Impl;
 
+import com.Java_project.Repository.TesteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TesteService {
+    @Autowired
+    TesteRepository testeRepository;
+
+    void teste (){
+         testeRepository.findAll();
+    }
 }
