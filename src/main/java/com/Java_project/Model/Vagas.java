fc06811fd588a0ab12a734ff2a696e5a0e30e555
Rinaldo Reverte Mendes Junior <rinaldojunior2012@gmail.com>
2024-01-03 20:55:29 -0400

    feat: Criado objetos da modelagem inicial.

---
+++ src/main/java/com/Java_project/Model/Vagas.java
@@ -0,0 +1,17 @@
+package com.Java_project.Model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Vagas {
+
+    String nome;
+
+    String descricao;
+
+    List<Requisitos> requisitosList;
+
+    Date prazoVaga;
+
+
+}
