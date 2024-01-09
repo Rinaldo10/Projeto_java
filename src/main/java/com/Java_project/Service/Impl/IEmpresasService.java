dc46eca80469430e2307ea15fe49c0ca3d5a15d3
Rinaldo Reverte Mendes Junior <rinaldojunior2012@gmail.com>
2024-01-08 20:55:10 -0400

    feat: Adicionado esqueleto inicial dos metodos do crud.

---
+++ src/main/java/com/Java_project/Service/Impl/IEmpresasService.java
@@ -0,0 +1,11 @@
+package com.Java_project.Service.Impl;
+
+public interface IEmpresasService{
+
+    void getEmpresa();
+    void patchEmpresa();
+    void deleteEmpresa();
+    void postEmpresa();
+
+
+}
