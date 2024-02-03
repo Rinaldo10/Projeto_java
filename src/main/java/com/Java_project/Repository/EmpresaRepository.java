f19b8b6abfb78061fd16bb4f89e1ef45acb855bf
Rinaldo Reverte Mendes Junior <rinaldojunior2012@gmail.com>
2024-02-02 20:54:30 -0400

    feat: Adicao de metodo para editar Empresa.

--- src/main/java/com/Java_project/Repository/EmpresaRepository.java
+++ src/main/java/com/Java_project/Repository/EmpresaRepository.java
@@ -1,11 +1,16 @@
 package com.Java_project.Repository;
 
+import com.Java_project.DTO.EditEmpresaRequest;
 import com.Java_project.Model.Empresa;
 import com.Java_project.Model.Requisito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    @Query
+    Empresa updateEmpresa(EditEmpresaRequest request, Long id);
 
 }
